30aaac86a61eab38bc764e396fd39464910d3928
gandresto <gandresto10@hotmail.com>
2019-08-27 12:52:10 -0500

    Reordenar función crear.

--- Ejercicios/pApArApArApCd.cpp
+++ Ejercicios/pApArApArApCd.cpp
@@ -4,6 +4,21 @@
 
 using namespace std;
 
+char *(*(*CreaFnApArApArApCd (void))[2])[2];
+
+int main(void){
+    // p es un Apuntador a un Arreglo de Apuntadores de 
+    // Arreglos a Apuntadores a Cadenas
+    char *(*(*pApArApArApCd)[2])[2] = CreaFnApArApArApCd();
+    for (int i = 0; i < 2; i++)
+        for (int j = 0; j < 2; j++)
+            // Cuando se imprimen cadenas, el último asterisco antes del tipo base se elimina
+            cout << /* * */(*(*pApArApArApCd)[i])[j] << " ";
+    cout << endl;
+    
+    return 0;
+}
+
 char *(*(*CreaFnApArApArApCd (void))[2])[2]{
     char *(*(*pApArApArApCd)[2])[2] = 
             (char*(*(*)[2])[2]) new (char *(*[2])[2]);
@@ -11,10 +26,10 @@ char *(*(*CreaFnApArApArApCd (void))[2])[2]{
     char **YApApCd = new (char * [2]);
     //Inicializando los arreglos de apuntadores a cadenas
     //Pueden tener tamaños diferentes
-    XApApCd[0] = new char[13];
-    XApApCd[1] = new char[14];
-    YApApCd[0] = new char[15];
-    YApApCd[1] = new char[16];
+    XApApCd[0] = new char[5];
+    XApApCd[1] = new char[6];
+    YApApCd[0] = new char[5];
+    YApApCd[1] = new char[7];
 
     strcpy(XApApCd[0], "Hola");
     strcpy(XApApCd[1], "mundo");
@@ -25,15 +40,4 @@ char *(*(*CreaFnApArApArApCd (void))[2])[2]{
     (*pApArApArApCd)[1] = (char *(*)[2]) YApApCd;
 
     return pApArApArApCd;
-}
-
-int main(void){
-    char *(*(*pApArApArApCd)[2])[2] = CreaFnApArApArApCd();
-    for (int i = 0; i < 2; i++)
-        for (int j = 0; j < 2; j++)
-            // Cuando se imprimen cadenas, el último asterisco antes del tipo base se elimina
-            cout << /* * */(*(*pApArApArApCd)[i])[j] << " ";
-    cout << endl;
-    
-    return 0;
 }
