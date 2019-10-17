a16e122de5ed0ac13b0954df7e7f0cb021a6889f
gandresto <gandresto10@hotmail.com>
2019-10-17 13:37:45 -0500

    Cambio los prints.

--- 2oExamenParcial/ejercicio1.cpp
+++ 2oExamenParcial/ejercicio1.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void ExhibeFn(int n){
     for(int i=1; i<=n; i++){
-        //cout << i << " ";
+        cout << i << " ";
         if(i%3==0 && i%5==0) cout << "Hola mundo" << endl;
         else if(i%3==0) cout << "Hola" << endl;
         else if(i%5==0) cout << "mundo" << endl;
